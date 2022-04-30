67cce411ccff0cf041aa1e6cfe4426f8a0380df3
AleksandorMart <99171200+AleksandorMart@users.noreply.github.com>
2022-05-01 00:26:44 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,14 +2,78 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+struct SYM {
+char ch;
+int prior;
 };
 
-struct SYM {
-  char ch;
-  int prior;
+template<typename T>
+class TPQueue {
+private:
+struct point {
+T inf;
+point* next = NULL;
+point(T zn){
+inf = zn;
+}
+};
+
+point* head;
+
+public:
+
+TPQueue(){
+head = NULL;
+}
+
+void push(T znach) {
+if (head == NULL) {
+head = new point(znach);
+}
+else {
+point* count = head;
+if (count->inf.prior < znach.prior)
+{
+head = new point(znach);
+head->next = count;
+}
+else
+{
+int f = 0;
+while (count->next != NULL)
+{
+if (count->next->inf.prior < znach.prior)
+{
+point* tmp = new point(znach);
+tmp->next = count->next;
+count->next = tmp;
+f = 1;
+break;
+}
+count = count->next;
+}
+if (f == 0)
+{
+count->next = new point(znach);
+}
+}
+}
+}
+
+T pop() {
+point* count = head;
+head = head->next;
+return count->inf;
+}
+
+~TPQueue(){
+T* del;
+while(head != NULL){
+del = head;
+head = head->next;
+delete del;
+}
+}
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
